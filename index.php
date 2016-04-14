2be39bfe6872dc123e8eb28b5972eb543d4d73bd
dehv <david.brunschier@gmail.com>
2016-04-14 23:32:16 +0200

    addaed php blablab

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Galilei Gymnasium</title>
+
+        <!--GFonts-->
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Lato:400,300,100' rel='stylesheet' type='text/css'>
+        <!--GFonts End-->
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/octicons/3.1.0/octicons.min.css">
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="index.css">
+
+        <!--[if lt IE 9]>
+            <script src="https://cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a href="#" class="pull-left"><img src="logo.png"></a>
+
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse,,">
+                <ul class="nav navbar-nav">
+                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                    <li>
+                        <a href="#page-top">Willkommen</a>
+                    </li>
+                    <li>
+                        <a href="aboutus.html">Über uns</a>
+                    </li>
+
+                      <li class="dropdown">
+                        <a class="dropdown-toggle Jahrgangstufe" data-toggle="dropdown" href="#">Jahrgangsstufen
+                          <span class="caret"></span></a>
+                          <ul class="dropdown-menu">
+                            <li><a href="unterstufe.html">Unterstufe</a></li>
+                            <li><a href="mittelstufe.html">Mittelstufe</a></li>
+                            <li><a href="oberstufe.html">Oberstufe</a></li>
+                          </ul>
+                        </li>
+                    <li>
+                        <a href="schulleben.html">Schulleben</a>
+                    </li>
+                    <li>
+                      <a href="blog.html">Blog</a>
+                    </li>
+                    <li>
+                      <a href="contact.html">Kontakt</a>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+        <div class="visible-lg-block visible-md-block">
+            <div class="main">
+                <!-- Video Demo
+                <div style="width: 100%; height: 100vh;" data-vide-bg="stockvideo.mp4" data-vide-options="loop: true, muted: true, autoplay: true, posterType: 'none', position: 50% 50%">
+                    <h2>Sample text</h2>
+                </div>
+                -->
+
+                <div class="text">
+                  <span class="gal">Galilei Gymnasium</span> <br>
+                  <span class="hamm">Hamm</span>
+                </div>
+            </div>
+            <div class="pinned" id="pin">
+                Pinned
+            </div>
+            <div class="posts">
+                Posts
+
+                <div class="sidebar">
+                Sidebar
+                </div>
+            </div>
+
+        </div>
+
+
+
+
+        <script src="https://cdn.jsdelivr.net/jquery/2.1.3/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <script src="jquery.vide.js"></script>
+        <script src="jquery.easing.min.js"></script>
+        <script src="index.js"></script>
+    </body>
+</html>
